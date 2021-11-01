8f64a78d9284fc94c1d713adb7c47b9d8a384319
LeoMTal <leonardo_talarico@hotmail.com>
2021-11-01 17:55:42 -0300

    Finalização do portfólio

--- js/slideShowImages.js
+++ js/slideShowImages.js
@@ -28,3 +28,124 @@ var swiper = new Swiper(".recent-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".socialMedia-slider", {
+  loop: true,
+  slidesPerView: 6,
+  spaceBetween: 0,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    clickable: true
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+var swiper = new Swiper(".digitalPainting-slider", {
+  loop: true,
+  slidesPerView: 6,
+  spaceBetween: 0,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    clickable: true
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+var swiper = new Swiper(".identidadeVisual-slider", {
+  loop: true,
+  slidesPerView: 6,
+  spaceBetween: 0,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    clickable: true
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1500: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+var swiper = new Swiper(".businessPartners-slider", {
+  loop: true,
+  centeredSlides:true,
+  spaceBetween: 10,
+  slidesPerView: 5,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    clickable: true
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 5,
+    }
+  },
+});
